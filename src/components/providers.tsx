40f5abc027e2abe7098fc7b9dddc5ccb6bdc5e01
Daniel Craciun <danielcraciun111@yahoo.com>
2024-07-15 17:51:32 +0100

    feat: providers.tsx fixed to allow injection of providers

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,6 +3,11 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
-export function Provider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function Provider({ children, ...externalProps }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider {...externalProps}>
+      {children}
+      {/* // ! Leave this comment to ensure the plugins inject correctly. */}
+    </NextThemesProvider>
+  )
 }
