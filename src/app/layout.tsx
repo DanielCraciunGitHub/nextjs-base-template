1460d6753c0dfee06bfefc8cb9c9abe279fae248
Daniel Craciun <danielcraciun111@yahoo.com>
2024-07-15 17:51:32 +0100

    feat: providers.tsx fixed to allow injection of providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import "@/styles/globals.css"
 
 import NextTopLoader from "nextjs-toploader"
 
-import { Provider } from "@/components/providers"
+import { NextInjectProvider } from "@/components/next-inject-providers"
 
-export default function RootLayout({
+export default function rootLayout({
   children,
 }: {
   children: React.ReactNode
@@ -14,9 +14,9 @@ export default function RootLayout({
       <body className="flex min-h-screen flex-col">
         {/* // ! This component shows a green loading bar when traversing internal pages in your site */}
         <NextTopLoader showSpinner={false} color="green" />
-        <Provider attribute="class" defaultTheme="dark" enableSystem>
+        <NextInjectProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
-        </Provider>
+        </NextInjectProvider>
       </body>
     </html>
   )
